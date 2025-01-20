0336569355bf47351c2a168ce1837a4ee7100ef8
cbay-soft <160279235+cbay-soft@users.noreply.github.com>
2025-01-20 09:52:54 -0500

    Update create-record.blade.php

--- resources/views/vendor/filament-panels/resources/pages/create-record.blade.php
+++ resources/views/vendor/filament-panels/resources/pages/create-record.blade.php
@@ -4,6 +4,34 @@
         'fi-resource-' . str_replace('/', '-', $this->getResource()::getSlug()),
     ])
 >
+
+    <link href="https://fonts.cdnfonts.com/css/adventure-request" rel="stylesheet">
+    <style>
+        body {
+            background-color: rgb(9, 64, 82) !important; /* Fondo base */
+            font-family: 'Adventure Request' !important; /* Fuente personalizada */
+            background-repeat: no-repeat; /* Sin repetir */
+            background-position: c  enter; /* Centrar imagen */
+            background-size: contain; /* Ajusta la imagen dentro del contenedor */
+            position: relative;
+        }
+
+        body::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: url('/images/logoGamerFest1.png'); /* Imagen de fondo */
+            background-repeat: no-repeat; /* Evitar repetición de la imagen */
+            background-position: center; /* Centrar la imagen */
+            background-size: contain; /* Ajusta la imagen dentro del contenedor */
+            opacity: 0.05; /* Controla la visibilidad de la marca de agua */
+            z-index: -1; /* Asegura que la imagen esté detrás del contenido */
+        }
+    </style>
+    
     <x-filament-panels::form
         id="form"
         :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()"
